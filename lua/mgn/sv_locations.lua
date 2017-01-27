252878691972eff4e8de283c2720b7dda2814215
Daniel <danielga@outlook.pt>
2017-01-27 23:02:38 +0000

    Fixed light/sirens locations and made them use landmarks.

--- lua/mgn/sv_locations.lua
+++ lua/mgn/sv_locations.lua
@@ -1,92 +1,92 @@
 mgn.AlarmLocations = {
 	-- Lobby
-	{Position = Vector(-14461, -2010, 14426), Normal = Vector(0, 1, 0)},
-	{Position = Vector(-11994, -2096, 14454), Normal = Vector (0, -1, 0)},
-	{Position = Vector(-13541, 33, 14441), Normal = Vector(-1, 0, 0)},
-	{Position = Vector(-14644, 703, 14422), Normal = Vector(0, -1, 0)},
-	{Position = Vector(-14639, 2000, 14475), Normal = Vector(0, 1, 0)},
+	{Position = LMVector(-373, -623, 274, "lobby", true):pos(), Normal = Vector(0, 1, 0)},
+	{Position = LMVector(0, 64, 118, "club", true):pos(), Normal = Vector (0, -1, 0)},
+	{Position = LMVector(-960, 720, 450, "armory", true):pos(), Normal = Vector(-1, 0, 0)},
+	{Position = LMVector(-556, 2089, 110, "lobby", true):pos(), Normal = Vector(0, -1, 0)},
+	{Position = LMVector(1, -992, -409, "reactor", true):pos(), Normal = Vector(0, 1, 0)},
 
 	-- RP
-	{Position = Vector(-8088, 12800, -13153), Normal = Vector(0, -1, 0)},
+	{Position = LMVector(1139, 845, -41, "land_rp", true):pos(), Normal = Vector(0, -1, 0)},
 
 	-- Fishing
-	{Position = Vector(4097, 8640, 12343), Normal = Vector(0, -1, 0)},
-	{Position = Vector(11954, -312, 12478), Normal = Vector(0, 1, 0)},
+	{Position = LMVector(-228, -1566, 75, "land_fish1", true):pos(), Normal = Vector(0, -1, 0)},
+	{Position = LMVector(-77, -296, 205, "land_fish2", true):pos(), Normal = Vector(0, 1, 0)},
 
 	-- Soccer
-	{Position = Vector(1880, -7696, 4216), Normal = Vector(0, -1, 0)},
+	{Position = LMVector(-33, 174, 120, "soccer", true):pos(), Normal = Vector(0, -1, 0)},
 
 	-- Build
-	{Position = Vector(-4464, 2131, -13084), Normal = Vector(1, 0, 0)},
-	{Position = Vector(6669, 6208, -12948), Normal = Vector(0, 1, 0)},
-	{Position = Vector(11254, -5232, -13431), Normal = Vector(0, 1, 0)},
-	{Position = Vector(10048, -489, -13085), Normal = Vector(-1, 0, 0)},
-	{Position = Vector(-2076, 640, -13228), Normal = Vector(0, 1, 0)},
-	{Position = Vector(-15000, 7842, -13051), Normal = Vector(0.7, -0.7, 0)},
-	{Position = Vector(-9247, -10621, -8351), Normal = Vector(0, 1, 0)},
+	{Position = LMVector(-74, -916, 2764, "land_vphys", true):pos(), Normal = Vector(1, 0, 0)},
+	{Position = LMVector(-2195, 352, 2205, "Smooth", true):pos(), Normal = Vector(0, 1, 0)},
+	{Position = LMVector(-10, 2704, 736, "Silo", true):pos(), Normal = Vector(0, 1, 0)},
+	{Position = LMVector(-908, 105, 2166, "land_caves", true):pos(), Normal = Vector(-1, 0, 0)},
+	{Position = LMVector(101, 482, 68, "bunker", true):pos(), Normal = Vector(0, 1, 0)},
+	{Position = LMVector(-4498, -5020, 75, "land_rp", true):pos(), Normal = Vector(1, 0, 0)},
+	{Position = LMVector(213, -3226, -11171, "AutoInstance0flood", true):pos(), Normal = Vector(0, 1, 0)},
 
 	-- Build basement
-	{Position = Vector(-6562, 2864, -15783), Normal = Vector(0, 1, 0)},
+	{Position = LMVector(-964, -1068, 13, "classroom", true):pos(), Normal = Vector(0, 1, 0)},
 
 	-- Build caves
-	{Position = Vector(8861, 4016, -15178), Normal = Vector (0, -1, 0)}
+	{Position = LMVector(-3, -1840, -25, "Smooth", true):pos(), Normal = Vector (0, -1, 0)}
 }
 
 mgn.LightLocations = {
 	-- Lobby
-	{Position = Vector(-13244, -811, 14313), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 150, Depth = 3000, SpriteSize = 32, LightCount = 30},
-	{Position = Vector(-13019, 96, 14308), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 300, SpriteSize = 32, LightCount = 6},
-	{Position = Vector(-15437, -1314, 14308), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 800, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(-14160, 567, 14308), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 200, Depth = 3000, SpriteSize = 32, LightCount = 30},
-	{Position = Vector(-15999, -118, 14308), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 200, Depth = 3000, SpriteSize = 32, LightCount = 30},
-	{Position = Vector(-14417, -1445, 14308), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 150, Depth = 3000, SpriteSize = 32, LightCount = 30},
-	{Position = Vector(-14527, -3374, 13956), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 2200, SpriteSize = 32, LightCount = 30},
-	{Position = Vector(-13579, -2745, 14160), Angles = Angle(25, -180, 0), DoubleStriped = true, Width = 80, Depth = 500, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(-13946, -2747, 14041), Angles = Angle(25, 180, 0), DoubleStriped = true, Width = 80, Depth = 500, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(-11995, -2232, 14308), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(-12859, -1535, 14308), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 50, Depth = 1000, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(-14402, -2628, 14213), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(-15921, -1800, 14308), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(-14642, 2143, 14308), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 250, Depth = 500, SpriteSize = 32, LightCount = 6},
-	{Position = Vector(-14644, 1172, 14308), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 70, Depth = 1000, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(-12666, 576, 14308), Angles = Angle(0, -180, 0), DoubleStriped = true, Width = 200, Depth = 1900, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(-13275, 32, 14308), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 200, Depth = 500, SpriteSize = 32, LightCount = 6},
-	{Position = Vector(-15668, 138, 14316), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
-	{Position = Vector(-14870, -348, 14316), Angles = Angle(0, 180, 0), DoubleStriped = false, Width = 0, Depth = 1000, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(-15236, -801, 14316), Angles = Angle(0, -90, 0), DoubleStriped = false, Width = 0, Depth = 2000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-663, -124, 322, "armory", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 150, Depth = 3000, SpriteSize = 32, LightCount = 30},
+	{Position = LMVector(-667, -480, -4, "minigame", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 300, SpriteSize = 32, LightCount = 6},
+	{Position = LMVector(-1349, 72, -4, "lobby", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 800, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(-1808, -9, -4, "minigame", true):pos(), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 200, Depth = 3000, SpriteSize = 32, LightCount = 30},
+	{Position = LMVector(-1911, 1268, -4, "lobby", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 200, Depth = 3000, SpriteSize = 32, LightCount = 30},
+	{Position = LMVector(-329, -59, -4, "lobby", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 150, Depth = 3000, SpriteSize = 32, LightCount = 30},
+	{Position = LMVector(-625, -302, -308, "land_theater", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 2200, SpriteSize = 32, LightCount = 30},
+	{Position = LMVector(323, 327, -104, "land_theater", true):pos(), Angles = Angle(25, -180, 0), DoubleStriped = true, Width = 80, Depth = 500, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(-44, 325, -223, "land_theater", true):pos(), Angles = Angle(25, 180, 0), DoubleStriped = true, Width = 80, Depth = 500, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(-1, -72, -28, "club", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-460, 4, -4, "sauna", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 50, Depth = 1000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-500, 444, -51, "land_theater", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-258, 1269, -37, "ccal", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-2, -849, -576, "reactor", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 250, Depth = 500, SpriteSize = 32, LightCount = 6},
+	{Position = LMVector(-4, -1820, -576, "reactor", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 70, Depth = 1000, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(-314, 0, -4, "minigame", true):pos(), Angles = Angle(0, -180, 0), DoubleStriped = true, Width = 200, Depth = 1900, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-694, 719, 317, "armory", true):pos(), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 200, Depth = 500, SpriteSize = 32, LightCount = 6},
+	{Position = LMVector(-1580, 1524, 4, "lobby", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
+	{Position = LMVector(-782, 1038, 4, "lobby", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = false, Width = 0, Depth = 1000, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(-1148, 585, 4, "lobby", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = false, Width = 0, Depth = 2000, SpriteSize = 32, LightCount = 15},
 
 	-- Build
-	{Position = Vector(-3943, 2133, -13294), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 100, Depth = 2000, SpriteSize = 128, LightCount = 15},
-	{Position = Vector(-5213, 2938, -13692), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 100, Depth = 2000, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(-6102, 2012, -13812), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
+	{Position = LMVector(-1729, 1991, -206, "land_build", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 100, Depth = 2000, SpriteSize = 128, LightCount = 15},
+	{Position = LMVector(-823, -109, 2156, "land_vphys", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 100, Depth = 2000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-504, -1920, 1985, "classroom", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
 
 	-- RP
-	{Position = Vector(-8078, 12402, -13302), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 64, LightCount = 10},
-	{Position = Vector(-11158, 12292, -13304), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
-	{Position = Vector(-5401, 12289, -13304), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
+	{Position = LMVector(1140, 521, -183, "land_rp", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 64, LightCount = 10},
+	{Position = LMVector(-1410, 331, -185, "land_rp", true):pos(), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
+	{Position = LMVector(3747, 328, -185, "land_rp", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
 
 	-- Build basement
-	{Position = Vector(-6020, 3911, -15852), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 200, Depth = 1000, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(-6476, 3584, -15852), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 200, Depth = 2000, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(-5800, 3019, -15852), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 200, Depth = 2000, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(-5005, 3001, -15851), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
-	{Position = Vector(-3971, 3040, -15852), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 200, Depth = 1000, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(-422, -21, -55, "classroom", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 200, Depth = 1000, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(-878, -348, -55, "classroom", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 200, Depth = 2000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-202, -913, -55, "classroom", true):pos(), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 200, Depth = 2000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-615, -46, -3, "land_vphys", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
+	{Position = LMVector(419, -7, -4, "land_vphys", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 200, Depth = 1000, SpriteSize = 32, LightCount = 10},
 
 	-- Build
-	{Position = Vector(1316, 7639, -13304), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
-	{Position = Vector(6563, 6630, -13303), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 300, Depth = 1000, SpriteSize = 64, LightCount = 10},
-	{Position = Vector(-7926, 7635, -13304), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
-	{Position = Vector(-14749, 4481, -13298), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
-	{Position = Vector(-11409, 1502, -13304), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
-	{Position = Vector(1263, 1508, -13304), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
+	{Position = LMVector(-7388, -1129, -112, "Beach", true):pos(), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
+	{Position = LMVector(-2141, -2138, -111, "Beach", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 300, Depth = 1000, SpriteSize = 64, LightCount = 10},
+	{Position = LMVector(-2328, 3703, 2493, "classroom", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
+	{Position = LMVector(-4001, -7940, -179, "land_rp", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
+	{Position = LMVector(-5811, -2430, 2493, "classroom", true):pos(), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
+	{Position = LMVector(3440, 1350, -8, "bunker", true):pos(), Angles = Angle(0, 180, 0), DoubleStriped = true, Width = 300, Depth = 10000, SpriteSize = 64, LightCount = 30},
 
 	-- Fishing
-	{Position = Vector(4507, 8900, 12268), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(11939, -3, 12212), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(12648, -863, 12276), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
-	{Position = Vector(11954, -695, 12212), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
-	{Position = Vector(9602, -816, 12212), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 200, Depth = 2000, SpriteSize = 32, LightCount = 15},
-	{Position = Vector(9593, 534, 12212), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 200, Depth = 800, SpriteSize = 32, LightCount = 10},
-	{Position = Vector(4350, 8571, 12267), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 200, SpriteSize = 64, LightCount = 4},
-	{Position = Vector(4194, 9520, 12272), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 100, Depth = 400, SpriteSize = 64, LightCount = 5}
+	{Position = LMVector(182, -1306, 0, "land_fish1", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(-92, 13, -61, "land_fish2", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 100, Depth = 1000, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(617, -847, 3, "land_fish2", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
+	{Position = LMVector(-77, -679, -61, "land_fish2", true):pos(), Angles = Angle(0, 0, 0), DoubleStriped = true, Width = 100, Depth = 500, SpriteSize = 32, LightCount = 6},
+	{Position = LMVector(-2429, -800, -61, "land_fish2", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 200, Depth = 2000, SpriteSize = 32, LightCount = 15},
+	{Position = LMVector(-2438, 550, -61, "land_fish2", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 200, Depth = 800, SpriteSize = 32, LightCount = 10},
+	{Position = LMVector(25, -1635, -1, "land_fish1", true):pos(), Angles = Angle(0, 90, 0), DoubleStriped = true, Width = 100, Depth = 200, SpriteSize = 64, LightCount = 4},
+	{Position = LMVector(-131, -686, 4, "land_fish1", true):pos(), Angles = Angle(0, -90, 0), DoubleStriped = true, Width = 100, Depth = 400, SpriteSize = 64, LightCount = 5}
 }
